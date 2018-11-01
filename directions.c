b47c8ed39f6491a1e4006422b2241fe624740eed
aidris823 <pokeanand@gmail.com>
2018-11-01 11:01:56 -0400

    Started dir info, makefile, .h, make run works

--- directions.c
+++ directions.c
@@ -6,10 +6,17 @@
 #include <errno.h>
 
 int main(){
-  DIR * dir_path;
+  DIR *dir_path;
   dir_path = opendir("./");
   dir_info(dir_path);
   return 0;
 }
 
-dir_info(DIR * dir_path){
+void dir_info(DIR * dir_path){
+  struct dirent *stuff;
+  stuff = readdir(dir_path);
+  closedir(dir_path);
+  while(stuff);
+  char *name = stuff->d_name;
+  printf("Name of file: %s\n",name);
+}
---
+++ directions.h
@@ -0,0 +1,1 @@
+void dir_info(DIR * dir_path);
