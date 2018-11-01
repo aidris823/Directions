e0ce441f96f5b35da0352e0283298c84aa8207e4
Meredith <mhu2@stuy.edu>
2018-11-01 15:06:32 -0400

    worked on print a bit, but it has an infinite loop.

--- directions.c
+++ directions.c
@@ -4,19 +4,25 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include "directions.h"
 
 int main(){
   DIR *dir_path;
-  dir_path = opendir("./");
-  dir_info(dir_path);
+  if(dir_path = opendir("./")){
+    dir_info(dir_path);
+  }else{
+    printf("didn't work\n");
+  }
   return 0;
 }
 
 void dir_info(DIR * dir_path){
   struct dirent *stuff;
   stuff = readdir(dir_path);
+  while(stuff){
+    printf("Name of file: %s\n",stuff->d_name);
+  }
   closedir(dir_path);
-  while(stuff);
-  char *name = stuff->d_name;
-  printf("Name of file: %s\n",name);
+  //char *name = stuff->d_name;
+  //printf("Name of file: %s\n",name);
 }
